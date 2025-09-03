f1c54f9d6cda10008523311f8114a9fd1b69a27e
Dmitrii <dgorbacev742@gmail.com>
2025-09-03 15:17:43 +0300

    add config server

---
+++ infrastructure/config-server/src/main/java/com/reactiverates/config/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.reactiverates.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Config Server Application
+ * 
+ * Централизованное управление конфигурациями микросервисов
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
